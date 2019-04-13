1015dc3516a3c2d231f990815ac89dac303e83f4
Kryshenyk Dmytro <KryshenykDmytro@gmail.com>
2019-04-13 11:59:46 +0300

    created examples_slider number pagination

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -1,10 +1,15 @@
 "use strict";
 
-$('.examples_slider').slick({
-  dots: false //   autoplay: true, 
-  //   autoplaySpeed: 4000,
-
-});
 $(window).resize(function () {
-  $('.examples-slider')[0].slick.refresh();
+  $('.examples_slider')[0].slick.refresh();
 });
+var $status = $('.examples_status');
+var $count = $('.examples_count');
+var $slickElement = $('.examples_slider');
+$slickElement.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
+  //currentSlide is undefined on init -- set it to 0 in this case (currentSlide is 0 based)
+  var i = (currentSlide ? currentSlide : 0) + 1;
+  $status.text('0' + i);
+  $count.text('0' + slick.slideCount);
+});
+$('.examples_slider').slick({});
