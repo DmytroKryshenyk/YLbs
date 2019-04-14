9ad2ce0f8ea91af0c06585739ac6df4130a39ae5
Kryshenyk Dmytro <KryshenykDmytro@gmail.com>
2019-04-14 12:29:59 +0300

    refactored the code

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -7,7 +7,6 @@ var $status = $('.examples_status');
 var $count = $('.examples_count');
 var $slickElement = $('.examples_slider');
 $slickElement.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
-  //currentSlide is undefined on init -- set it to 0 in this case (currentSlide is 0 based)
   var i = (currentSlide ? currentSlide : 0) + 1;
   $status.text('0' + i);
   $count.text('0' + slick.slideCount);
